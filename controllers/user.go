a73caaed0aaaffd72ad148f3012b6789ab9d7169
soniaray <sonia@caseable.com>
2019-07-28 21:30:54 +0200

    Adding CRUD functionality

--- controllers/user.go
+++ controllers/user.go
@@ -1,20 +1,85 @@
 package controllers
 
 import (
+	"encoding/json"
+	"go-webservice/models"
 	"net/http"
 	"regexp"
 )
 
 type UserController struct {
-	userIDPatter *regexp.Regexp
+	userIDPattern *regexp.Regexp
 }
 
 func (uc UserController) ServeHTTP (w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Hello from the User Controller!!!!"))
 }
 
+func (uc *UserController) getAll(w http.ResponseWriter, r *http.Request)  {
+	encondeResponseAsJSON(models.GetUsers(), w)
+}
+
+func (uc *UserController) get (id int, w http.ResponseWriter)  {
+	u, err := models.GetUserByID(id)
+	if err !=nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	encondeResponseAsJSON(u, w)
+}
+
+func (uc *UserController) post(w http.ResponseWriter, r *http.Request) {
+	u, err := uc.parseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not parse User object"))
+		return
+	}
+	u, err = models.AddUser(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encondeResponseAsJSON(u, w)
+}
+
+func (uc *UserController) put(id int, w http.ResponseWriter, r *http.Request) {
+	u, err := uc.parseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not parse User object"))
+		return
+	}
+	if id != u.id {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("ID of submitted user must match ID in URL"))
+		return
+	}
+	encondeResponseAsJSON(u, w)
+}
+
+func (uc *UserController) delete(id int, w http.ResponseWriter)  {
+	err := models.RemoveUserById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (uc *UserController) parseRequest(r *http.Request) (models.User, error) {
+	dec := json.NewDecoder(r.Body)
+	var u models.User
+	err := dec.Decode(&u)
+	if err != nil {
+		return models.User{}, err
+	}
+	return u, nil
+}
+
 func newUserController() *UserController {
 	return &UserController{
-		userIDPatter : regexp.MustCompile(`^/users/(\d+)/?`),
+		userIDPattern : regexp.MustCompile(`^/users/(\d+)/?`),
 	}
 }
--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	Id int
 	Firstname string
@@ -16,8 +21,40 @@ func GetUsers() []*User {
 }
 
 func AddUser(u User) (User, error) {
+	if u.Id != 0 {
+		return User{}, errors.New("New User must not include id or it must be set to zero")
+	}
 	u.Id = nextId
 	nextId++
 	users = append(users, &u)
 	return u, nil
+}
+
+func GetUserByID(id int) (User,error){
+	for _, u:= range users {
+		if u.Id == id {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.Id == u.Id {
+			users[i] = &u
+		}
+	}
+
+	return User{}, fmt.Errorf("User with ID '%v' not found", u.Id)
+}
+
+func RemoveUserById(id int) error {
+	for i, u := range users {
+		if u.Id == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID '%v' not found", id)
 }
